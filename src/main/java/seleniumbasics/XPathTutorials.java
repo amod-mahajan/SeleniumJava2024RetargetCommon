90d9250c9bd8d352758f7ddb516be0cdf492e436
amodm <retargetcommon@gmail.com>
2024-05-31 23:58:46 -0400

    Till video 34

--- src/main/java/seleniumbasics/XPathTutorials.java
+++ src/main/java/seleniumbasics/XPathTutorials.java
@@ -10,12 +10,10 @@ public class XPathTutorials {
     public static void main(String[] args) {
 
         ChromeDriver chromeDriver = new ChromeDriver();
-        chromeDriver.get("C:\\Users\\amodm\\IdeaProjects\\SeleniumJava2024RetargetCommon\\src\\main\\resources\\htmlpages\\XPathTutorials.html");
+        chromeDriver.get("C:\\Users\\amodm\\IdeaProjects\\SeleniumJava2024RetargetCommon\\src\\main\\resources\\htmlpages\\NonBreakingSpace.html");
         chromeDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-        //System.out.println(chromeDriver.findElement(By.xpath("//p[normalize-space()='This\u00a0\u00a0 is']")).getText());
-
-        //System.out.println(chromeDriver.findElement(By.xpath("//p[normalize-space(translate(.,'\u00a0',' '))='This is']")).getText());
+        System.out.println(chromeDriver.findElement(By.xpath("//a[normalize-space(translate(., '\u00A0', ' '))='Link With non-breaking Spaces between words Link']")).getText());
 
         chromeDriver.quit();
 
